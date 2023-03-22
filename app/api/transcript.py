51528cdfbc6d15b35b12e52f5a2ef70894e0aa3d
raul <123352622+rcodes7@users.noreply.github.com>
2023-03-21 22:59:08 -0500

    generate transcript from file

--- app/api/transcript.py
+++ app/api/transcript.py
@@ -1,7 +1,23 @@
-from flask import jsonify, request, current_app
+from flask import jsonify, request, make_response
 from . import api
+import whisper
+import tempfile
 
 
-@api.route('/transcript/')
-def get_transcipt():
-    return 'hello'
+@api.route('/transcript/', methods=['POST'])
+def generate_transcipt():
+    if 'file' not in request.files:
+        return make_response(jsonify({'error': 'No file part'}), 400)
+    
+    _file = request.files['file']
+    
+    if not _file or not _file.filename:
+        return make_response(jsonify({'error': 'No file selected'}), 400)
+    
+    model = whisper.load_model('base')
+    with tempfile.NamedTemporaryFile(delete=True) as temp:
+        _file.save(temp.name)
+        result = model.transcribe(temp.name)    
+
+    
+    return make_response(jsonify({'transcript': result['text']}), 200)
