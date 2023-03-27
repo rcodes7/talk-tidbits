f77a2e53de080288aaaa213b4421d9d557521ece
raul <123352622+rcodes7@users.noreply.github.com>
2023-03-26 21:43:59 -0500

    create models for transcriptions and summaries

--- app/models.py
+++ app/models.py
@@ -1,2 +1,24 @@
 from . import db
 
+class Transcription(db.Model):
+    __tablename__ = 'transcriptions'
+    id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(120), unique=True, nullable=False)
+    unique_id = db.Column(db.String(32), unique=True, nullable=False)
+    transcription = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    summaries = db.relationship('Summary', backref='transcription', lazy=True)
+
+    def __repr__(self):
+        return f'<Transcription {self.file_name}>'
+
+class Summary(db.Model):
+    __tablename__ = 'summaries'
+    id = db.Column(db.Integer, primary_key=True)
+    summary = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    transcription_id = db.Column(db.Integer, db.ForeignKey('transcriptions.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Summary {self.id}>'
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import click
 from flask_migrate import Migrate
 from app import create_app, db
+from app.models import Transcription, Summary
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate  = Migrate(app, db)
