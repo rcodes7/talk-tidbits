e3e56ebfe1a4b4a7373baf79dc2925ed747e56f0
raul <123352622+rcodes7@users.noreply.github.com>
2023-03-27 00:19:46 -0500

    persist whisper output

--- app/api/transcript.py
+++ app/api/transcript.py
@@ -1,10 +1,10 @@
 from flask import jsonify, request, make_response
+from uuid import uuid4
 from . import api
 from .. import db
-from ..models import Transcription
+from ..models import Transcription, Segment
 import whisper
 import tempfile
-
 
 @api.route('/transcript/', methods=['POST'])
 def generate_transcipt():
@@ -20,6 +20,11 @@ def generate_transcipt():
     with tempfile.NamedTemporaryFile(delete=True) as temp:
         _file.save(temp.name)
         result = model.transcribe(temp.name)
-
+        transcription = Transcription(file_name=_file.filename, transcription=result['text'], uuid=uuid4().hex)
+        db.session.add(transcription)
+        db.session.commit()
+        for segment in result['segments']:
+            db.session.add(Segment(segment=segment['text'], start=segment['start'], end=segment['end'], segment_id=segment['id'], transcription_id=transcription.id))
+        db.session.commit()
     
     return make_response(jsonify({'transcript': result['text']}), 200)
