53e576cc251fd5abd8121f49505f4c9e8450b3fb
raul <123352622+rcodes7@users.noreply.github.com>
2023-03-27 00:19:31 -0500

    add segments table to store segment output from whisper

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ class Transcription(db.Model):
     __tablename__ = 'transcriptions'
     id = db.Column(db.Integer, primary_key=True)
     file_name = db.Column(db.String(120), unique=True, nullable=False)
-    unique_id = db.Column(db.String(32), unique=True, nullable=False)
+    uuid = db.Column(db.String(32), unique=True, nullable=False)
     transcription = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
@@ -12,6 +12,19 @@ class Transcription(db.Model):
 
     def __repr__(self):
         return f'<Transcription {self.file_name}>'
+
+class Segment(db.Model):
+    __tablename__ = 'segments'
+    id = db.Column(db.Integer, primary_key=True)
+    segment = db.Column(db.Text, nullable=False)
+    segment_id = db.Column(db.Integer, nullable=False)
+    start = db.Column(db.Float, nullable=False)
+    end = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    transcription_id = db.Column(db.Integer, db.ForeignKey('transcriptions.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Segment {self.id}>'
 
 class Summary(db.Model):
     __tablename__ = 'summaries'
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import click
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import Transcription, Summary
+from app.models import Transcription, Summary, Segment
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
