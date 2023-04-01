44e84b17d07729af122d9b924560ecf2957feca4
raul <123352622+rcodes7@users.noreply.github.com>
2023-04-01 00:10:57 -0500

    add page layout

---
+++ talk-tidbits-frontend/components/layout.tsx
@@ -0,0 +1,27 @@
+import { PropsWithChildren } from 'react'
+import NavBar from './navbar'
+
+export default function PageLayout(props: PropsWithChildren) {
+  const items = ['Transcribe', 'Summmarize', 'Collections']
+  return (
+    <div className="drawer">
+      <input id="app-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        <NavBar items={items} drawerId={'app-drawer'} />
+        {props.children}
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="app-drawer" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-60 md:w-80 bg-base-100">
+          {items.map((item: string) => {
+            return (
+              <li key={`drawer-${item}`}>
+                <a>{item}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
